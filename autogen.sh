2086646727e586acd06c257ac450e9a31e36fd2c
Tommi Komulainen <tommi.komulainen@nokia.com>
2005-09-09 07:59:10 +0000

    also run configure

--- autogen.sh
+++ autogen.sh
@@ -6,3 +6,9 @@ aclocal-1.7 || aclocal
 autoconf
 autoheader
 automake-1.7 --add-missing --foreign || automake --add-missing --foreign
+
+if test x$NOCONFIGURE = x; then
+  ./configure --enable-maintainer-mode "$@"
+else
+  echo Skipping configure process.
+fi
