0483602bff646993a404deef36fca8ebb6dd8988
Michael Dominic Kostrzewa <michael.kostrzewa@nokia.com>
2007-08-21 08:10:40 +0000

    Replacing the "Done" button in the calendar popup with "Ok" and "Cancel". Fixes NB#59299.

--- src/hildon-calendar-popup.c
+++ src/hildon-calendar-popup.c
@@ -334,8 +334,8 @@ hildon_calendar_popup_init                      (HildonCalendarPopup *cal)
     gtk_box_pack_start (GTK_BOX (GTK_DIALOG (cal)->vbox), priv->cal,
             TRUE, TRUE, 0);
     gtk_dialog_set_has_separator (GTK_DIALOG (cal), FALSE);
-    gtk_dialog_add_button (GTK_DIALOG (cal), _("ecdg_bd_calendar_popout_done"),
-            GTK_RESPONSE_OK);
+    gtk_dialog_add_button (GTK_DIALOG (cal), _("ecdg_bd_font_dialog_ok"), GTK_RESPONSE_OK);
+    gtk_dialog_add_button (GTK_DIALOG (cal), _("ecdg_bd_wizard_cancel"), GTK_RESPONSE_CANCEL);
     gtk_widget_show(priv->cal);
 
     /* Connect signals */
