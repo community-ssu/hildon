25826ec641ab8df1c1c6a2cd7cff8a962d8fc9a8
Alejandro Piñeiro <apinheiro@igalia.com>
2009-04-02 13:04:45 +0000

    2009-04-02 Alejandro Pinheiro <apinheiro@igalia.com>
    * src/hildon-picker-dialog.c: removed HILDON_TOUCH_SELECTOR_HEIGHT
    	(_hildon_picker_dialog_set_selector): removed call to
    	gtk_widget_set_size_request
    	* src/hildon-touch-selector.c
    	(_create_new_column): change the pannable size request policy
    
    	Avoid to use a hardcoded height for the hildon picker dialog. In order
    	to get that, we needed to change the hildon pannable size request
    	policy
    	Fixes: NB#109369 (Hard-coded height in HildonPickerDialog)

--- src/hildon-picker-dialog.c
+++ src/hildon-picker-dialog.c
@@ -54,8 +54,6 @@
 
 G_DEFINE_TYPE (HildonPickerDialog, hildon_picker_dialog, HILDON_TYPE_DIALOG)
 
-#define HILDON_TOUCH_SELECTOR_HEIGHT            320
-
 struct _HildonPickerDialogPrivate
 {
   GtkWidget *selector;
@@ -584,10 +582,6 @@ _hildon_picker_dialog_set_selector (HildonPickerDialog * dialog,
                       dialog->priv->selector, TRUE, TRUE, 0);
 
   g_object_unref (selector);
-
-  /* Ensure that the dialog's height is correct */
-  gtk_widget_set_size_request (GTK_WIDGET (dialog->priv->selector), -1,
-                               HILDON_TOUCH_SELECTOR_HEIGHT);
 
   gtk_widget_show (dialog->priv->selector);
 
